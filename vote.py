4d6e15d2d05f45418b446b90b6abe95267eab84f
Sietse van der Molen <sietse@vdmolen.eu>
2015-06-23 12:49:17 +0200

    use a timer instead

--- vote.py
+++ vote.py
@@ -3,6 +3,7 @@ from telex import plugin
 
 import tgl
 import time
+import threading
 
 
 class VotePlugin(plugin.TelexPlugin):
@@ -11,7 +12,6 @@ class VotePlugin(plugin.TelexPlugin):
     """
 
     vote_in_progress = False
-    timer = 30
     votes_yes = 0
     votes_no = 0
 
@@ -26,36 +26,39 @@ class VotePlugin(plugin.TelexPlugin):
 
     def vote(self, msg, matches):
         peer = self.bot.get_peer_to_send(msg)
-        if vote_in_progress:
+        if self.vote_in_progress:
             cast_vote = matches.group(1)
             if cast_vote == "yes":
-                votes_yes += 1
+                self.votes_yes += 1
             elif cast_vote == "no":
-                votes_no += 1
+                self.votes_no += 1
             peer.send_msg("Vote recorded", reply=msg.id, preview=False)
         else:
             peer.send_msg("No vote in progress", reply=msg.id, preview=False)
 
     def call_vote(self, msg, matches):
         peer = self.bot.get_peer_to_send(msg)
-        if not vote_in_progress:
+
+        if not self.vote_in_progress:
+            self.vote_in_progress = True
             vote_msg = matches.group(1)
-            self.timer = matches.group(2) or 30
-            if timer > 600:
-                timer = 600
+            countdown_time = matches.group(2) or 30
+            if countdown_time > 600:
+                countdown_time = 600
 
+            self.timer = threading.Timer(countdown_time, self.close_vote, args=[msg, matches])
+            self.timer.start()
             peer.send_msg("VOTE NOW: {}".format(vote_msg), preview=False)
-            for i in range(timer, 0, -1):
-                if i == timer/2:
-                    peer.send_msg("{} seconds remaining".format(t), preview=False)
-                    peer.send_msg("Intermediate results:\n{} in favour\n{} against".format(votes_yes, votes_no))
-                elif i == 10:
-                    peer.send_msg("{} seconds remaining".format(t), preview=False)
-                elif i == 5:
-                    peer.send_msg("{} seconds remaining".format(t), preview=False)
-
-                time.sleep(1)
         else:
             peer.send_msg("Vote already in progress", reply=msg.id, preview=False)
 
-        peer.send_msg("Voting closes now! Results:\n{} voted yes\n{} voted no".format(votes_yes, votes_no), reply=msg.id, preview=False)
+    def close_vote(self, msg, matches):
+        peer = self.bot.get_peer_to_send(msg)
+        peer.send_msg("Voting closes now! Results:\n{} voted yes\n{} voted no".format(self.votes_yes, self.votes_no), reply=msg.id, preview=False)
+        self.reset_votes();
+
+
+    def reset_votes(self):
+        self.votes_yes = 0
+        self.votes_no = 0
+        self.vote_in_progress = False
